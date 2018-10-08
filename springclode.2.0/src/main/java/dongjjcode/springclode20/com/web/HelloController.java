6d8ffb5d46fda42f7c0fe0ec35de643b37ed9622
康东杰 <kangdongjie@cmhi.chinamobile.com>
2018-10-08 17:12:02 +0800

    springcloud test1

--- springclode.2.0/src/main/java/dongjjcode/springclode20/com/web/HelloController.java
+++ springclode.2.0/src/main/java/dongjjcode/springclode20/com/web/HelloController.java
@@ -1,4 +1,13 @@
 package dongjjcode.springclode20.com.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @RequestMapping("hello")
+    public String index() {
+        return "Hello World";
+    }
 }
