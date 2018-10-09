dbb83bdd6b2772a07ee74db8f51923d495672a8c
康东杰 <kangdongjie@cmhi.chinamobile.com>
2018-10-09 16:23:22 +0800

    eureka-discovery-client

---
+++ springcloud-eureka-discovery-client2/src/main/java/dongjjcode/springcloudeurekadiscoveryclient2/com/web/HelloController.java
@@ -0,0 +1,20 @@
+package dongjjcode.springcloudeurekadiscoveryclient2.com.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String index() {
+        System.out.println(client);
+        return "Hello World 8082";
+    }
+}
