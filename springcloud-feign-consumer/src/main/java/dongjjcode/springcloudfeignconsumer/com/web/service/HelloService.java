bd1af5658b8ba14af05705728faa5088ffd27b6f
康东杰 <kangdongjie@cmhi.chinamobile.com>
2018-10-09 17:08:49 +0800

    feign-consumer

---
+++ springcloud-feign-consumer/src/main/java/dongjjcode/springcloudfeignconsumer/com/web/service/HelloService.java
@@ -0,0 +1,11 @@
+package dongjjcode.springcloudfeignconsumer.com.web.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String hello();
+}
