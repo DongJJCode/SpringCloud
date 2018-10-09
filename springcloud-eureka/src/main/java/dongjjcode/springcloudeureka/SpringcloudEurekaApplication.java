82aae0fca310d84f2ca02a653df94a1907a2dc7e
康东杰 <kangdongjie@cmhi.chinamobile.com>
2018-10-09 16:28:20 +0800

    springcloud-eureka

--- springcloud-eureka/src/main/java/dongjjcode/springcloudeureka/SpringcloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/dongjjcode/springcloudeureka/SpringcloudEurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class SpringcloudEurekaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringcloudEurekaApplication.class, args);
 	}
 }
